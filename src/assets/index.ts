92204988d6563720a446f9bca2a3fc65da3f006f
sumantasau <sumantasau@gmail.com>
2021-04-21 09:45:59 +0530

    Modified login module with new ui

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -5,8 +5,9 @@ import shoe from './Shoe.png';
 import shirt from './Shirt.png';
 import trouser from './Trouser.png';
 import other from './Other.png';
-import google_login from './google.jpg';
-import facebook_login from './facebook.jpg';
+import google_login from './google.png';
+import facebook_login from './facebook.png';
+import loginleft from './loginleft.png'
 
 
-export { APP_LOGO, tshirt, shirt, shoe, trouser, other, AVATAR_IMAGE, google_login, facebook_login };
+export { APP_LOGO, tshirt, shirt, shoe, trouser, other, AVATAR_IMAGE, google_login, facebook_login, loginleft };
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,8 +1,9 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useRef } from 'react';
 import './Login.scss';
-import { APP_LOGO, google_login, facebook_login } from '../../assets';
+import { google_login, facebook_login, loginleft } from '../../assets';
 import { STATIC_DATA } from '../../config/StaticData';
 import FirebaseContext from '../Firebase/Context';
+import { AVATAR_IMAGE } from '../../assets';
 
 type ModalProps = {
   onCloseLoginModalClick: () => void;
@@ -12,14 +13,49 @@ export default function Login(props: ModalProps) {
 
   const firebase = useContext(FirebaseContext);
   const [errorMessage, setErrorMessage] = useState('');
+  const [displayMobile, setdisplayMobile] = useState(true);
+  const [displayProfile, setdisplayProfile] = useState(false);
 
   const {
     ENGLISH: {     
+      UserProfile: {       
+        AVATAR_ALT_TAG,
+      },
+      Login: {
+        LOGIN_HEADING,
+        LOGIN_MOBILE_TEXT,
+        LOGIN_MOBILE_PLACEHOLDER,
+        LOGIN_SENDOTP_BUTTON_TEXT,
+        LOGIN_INFO_TEXT,
+        LOGIN_OTP_TEXT,
+        LOGIN_OTP_PLACEHOLDER,
+        LOGIN_BUTTON_TEXT,
+        LOGIN_NAME_TEXT,
+        LOGIN_NAME_PLACEHOLDER,
+        LOGIN_PROFILE_BUTTON_TEXT,
+
+      },
       NO_SUCH_IMAGE,
     },
   } = STATIC_DATA;
 
+  const inputFile = useRef<HTMLInputElement>(null);
+
   const { onCloseLoginModalClick } = props;
+
+  const handleSendOTP = () => {
+    setdisplayMobile(false);    
+  }
+  const handleLogin = () => {
+    setdisplayProfile(true);
+  }
+
+  const onImageClick = () => {
+    if (inputFile.current !== null) {
+      inputFile.current.click();
+    }
+  };
+
 
   const handleGoogleSignIn = () => {
    
@@ -63,26 +99,73 @@ export default function Login(props: ModalProps) {
       })*/
       .catch((error:any) => {        
         setErrorMessage(error.message);
-        console.log(error.message);
+        console.log(errorMessage);
       });
   };
 
   return (
     <div id="login-container-overlay">
       <div className="login-container">
-          <div className="header-section">
-           <div>
-            <img src={APP_LOGO} alt={NO_SUCH_IMAGE}></img>  
-            </div>
-            <div>
-          <i className='fa fa-times fa-2x' onClick={onCloseLoginModalClick}></i>  
-          </div>
-          </div>
+          <div className="left-section">           
+              <img src={loginleft} alt={NO_SUCH_IMAGE}></img>  
+          </div>        
           <div className="login-section">
-            <div className="round-container">
-              <p> <img src={google_login} alt={NO_SUCH_IMAGE} onClick={handleGoogleSignIn}></img>  </p>     
-              <p><img src={facebook_login} alt={NO_SUCH_IMAGE} onClick={handleFacebookSignIn}></img>   </p>
+            <i className='fa fa-times fa-1x' onClick={onCloseLoginModalClick}></i>  
+            <h2>{LOGIN_HEADING}</h2>
+            {(displayProfile) ?
+              (<div className="input-section">
+              <p>{LOGIN_NAME_TEXT}</p>
+              <input type="text" placeholder={LOGIN_NAME_PLACEHOLDER}></input>
+              <p><img
+                src={AVATAR_IMAGE}
+                className='profile-image'
+                alt={AVATAR_ALT_TAG}
+                onClick={onImageClick}
+              />
+                <input
+                  type='file'
+                  id='file'
+                  ref={inputFile}
+                  className='profile-input-file'
+                />
+                </p>
+              <p>
+              <input type='button' value={LOGIN_PROFILE_BUTTON_TEXT} className='input-button'  />
+              </p>
+              
+            </div>)
+            :
+            ((displayMobile) ? 
+            <div className="input-section">
+              <p>{LOGIN_MOBILE_TEXT}</p>
+              <input type="text" placeholder={LOGIN_MOBILE_PLACEHOLDER}></input>
+              <p>
+              <input type='button' value={LOGIN_SENDOTP_BUTTON_TEXT} className='input-button' onClick={handleSendOTP} />
+              </p>
+              <h4>{LOGIN_INFO_TEXT}</h4>
             </div>
+              :
+            <div className="input-section">
+              <p>{LOGIN_OTP_TEXT}</p>
+              <input type="text" placeholder={LOGIN_OTP_PLACEHOLDER}></input>
+              <p>
+              <input type='button' value={LOGIN_BUTTON_TEXT} className='input-button' onClick={handleLogin} />
+              </p>
+              <h4>{LOGIN_INFO_TEXT}</h4>
+            </div>)
+          }
+          
+
+            <div className="division">
+              <hr></hr>
+              <p>Or sign in with</p>
+              <hr></hr>
+            </div>
+            <div className="social-login">
+            <img src={google_login} alt={NO_SUCH_IMAGE} onClick={handleGoogleSignIn}></img>
+            <img src={facebook_login} alt={NO_SUCH_IMAGE} onClick={handleFacebookSignIn}></img>
+            </div> 
+
           </div>
       </div>
     </div>
