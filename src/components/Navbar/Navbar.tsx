4451a3aba061a7ba003779bc63e3317ae8a41f5d
shubham <shubhamnarang89@gmail.com>
2021-04-23 20:45:10 +0530

    adds search functionality

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,9 @@
 import React, { FC, useState } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { APP_LOGO } from '../../assets';
 import { CONFIG } from '../../config/Config';
-import { STATIC_DATA } from '../../config/StaticData';
+import { STATIC_DATA, TEST_DATA } from '../../config/StaticData';
 import { NavbarModel } from '../../models/Navbar.model';
 import './Navbar.scss';
 
@@ -15,15 +15,68 @@ const Navbar: FC<NavbarModel.IProps> = ({ cartSize }) => {
     },
   } = STATIC_DATA;
   const {
-    ROUTES: { DASHBOARD, SHOPPING_CART },
+    ROUTES: { DASHBOARD, SHOPPING_CART, PRODUCT_DETAILS, PRODUCTS },
   } = CONFIG;
+  const { PRODUCTS_DATA } = TEST_DATA;
+  const history = useHistory();
 
   const [isSearchBarForMobileOpen, setIsSearchBarForMobileOpen] = useState(
     false
   );
+  const [searchTerm, setSearchTerm] = useState('');
 
   const handleSearchBar = () => {
     setIsSearchBarForMobileOpen(!isSearchBarForMobileOpen);
+  };
+  const getSearchedCategories = () => {
+    const categories = PRODUCTS_DATA.filter((data) =>
+      data.category.toLowerCase().includes(searchTerm.toLowerCase())
+    ).map(({ categoryId, category, images }) => {
+      return {
+        id: categoryId,
+        name: category,
+        image: images[0],
+        redirect: redirectToCategory,
+      };
+    });
+
+    const filteredCategories: any = [];
+    categories.forEach((data) => {
+      const selectedCat = filteredCategories.findIndex(
+        (fil: any) => fil.id === data.id
+      );
+      if (selectedCat === -1) {
+        filteredCategories.push(data);
+      }
+    });
+
+    return filteredCategories;
+  };
+
+  const getSearchedItems = () => {
+    const products = PRODUCTS_DATA.filter((data) =>
+      data.name.toLowerCase().includes(searchTerm.toLowerCase())
+    ).map(({ id, name, category, images }) => {
+      return {
+        id,
+        name,
+        category,
+        image: images[0],
+        redirect: redirectToProductDescription,
+      };
+    });
+
+    return products;
+  };
+
+  const redirectToProductDescription = (id: number) => {
+    setSearchTerm('');
+    history.push(`${PRODUCT_DETAILS}?pid=${id}`);
+  };
+
+  const redirectToCategory = (id: number, name: string) => {
+    setSearchTerm('');
+    history.push(`${PRODUCTS}?cid=${id}&cname=${name}`);
   };
 
   return (
@@ -35,7 +88,36 @@ const Navbar: FC<NavbarModel.IProps> = ({ cartSize }) => {
           </Link>
         </div>
         <div className='search-bar'>
-          <input type='text' placeholder={SEARCH_PLACEHOLDER}></input>
+          <input
+            type='text'
+            placeholder={SEARCH_PLACEHOLDER}
+            onChange={(event) => setSearchTerm(event.target.value)}
+            className={searchTerm ? 'plain-border' : 'round-border'}
+          ></input>
+          {searchTerm && (
+            <ul className='search-menu'>
+              {getSearchedCategories().map((item: any) => (
+                <li
+                  key={item.id}
+                  onClick={() => item.redirect(item.id, item.name)}
+                >
+                  <img src={item.image} alt={NO_SUCH_IMAGE}></img>
+                  <div>
+                    <h4>{item.name}</h4>
+                  </div>
+                </li>
+              ))}
+              {getSearchedItems().map((item) => (
+                <li key={item.id} onClick={() => item.redirect(item.id)}>
+                  <img src={item.image} alt={NO_SUCH_IMAGE}></img>
+                  <div>
+                    <h4>{item.name}</h4>
+                    <p>{item.category}</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
         <div className='user-div'>
           <i className='fa fa-user-circle'></i>
